fbd934452e53fd969fcaab8406a4e757c9697540
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-04 14:59:41 +0100

    search order by name

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,5 +7,12 @@ def index():
     return render_template('index.html', orders = orders)
 
 @app.route('/order/<index>')
-def one_item(index):
-    return render_template('order.html', order = orders[int(index)-1])
+def one_by_index(index):
+    return render_template('order_by_index.html', order = orders[int(index)-1])
+
+@app.route('/order/name/<name>')
+def one_by_name(name):
+    for order in orders:
+        if order.customer_name == name:
+            return render_template('order_by_name.html', order = order)
+            
