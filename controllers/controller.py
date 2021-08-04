c832bc35e5cf1aa00a8957520dec63e2aef62b33
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-04 14:41:57 +0100

    one order completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,6 +2,10 @@ from app import app
 from flask import render_template
 from models.order_list import orders
 
-@app.route('/')
+@app.route('/order')
 def index():
     return render_template('index.html', orders = orders)
+
+@app.route('/order/<index>')
+def one_item(index):
+    return render_template('order.html', order = orders[int(index)-1])
