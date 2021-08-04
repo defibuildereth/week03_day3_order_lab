cc8ff02461f9bb086a5884a43f89c76778d454e2
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-04 13:53:03 +0100

    MVP completed

--- models/order.py
+++ models/order.py
@@ -1,6 +1,7 @@
 class Order():
 
-    def __init__(self, customer_name, order_date, quantity):
+    def __init__(self, customer_name, order_date, quantity, description):
         self.customer_name = customer_name
         self.order_date = order_date
         self.quantity = quantity
+        self.description = description
--- models/order_list.py
+++ models/order_list.py
@@ -1,6 +1,6 @@
 from models.order import Order
  
-order_1 = Order('Jeremy', '04/08/2021', 3)
-order_2 = Order('Donald', '04/08/2021', 2)
+order_1 = Order('Jeremy', '04/08/2021', 3, 'Ping-pong bats')
+order_2 = Order('Donald', '04/08/2021', 2, 'Ping-pong balls')
 
 orders = [order_1, order_2]
